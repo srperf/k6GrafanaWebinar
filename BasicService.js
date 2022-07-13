bec72f66f276b22aa6395ebe7ac0623f6c49a94e
Señor Performo - Leandro Melendez <54183040+srperf@users.noreply.github.com>
2022-07-13 16:26:35 -0500

    Update BasicService.js

--- BasicService.js
+++ BasicService.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hola Mundo');
-},Math.floor(Math.random() * 10) + 1;);
+},Math.floor(Math.random() * 10) + 1);
 
 server.listen(port, hostname, () => {
   console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
