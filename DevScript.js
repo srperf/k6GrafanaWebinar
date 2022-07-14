237b70cf30262443a0dc93639ec1d85227424381
Señor Performo - Leandro Melendez <54183040+srperf@users.noreply.github.com>
2022-07-14 09:55:48 -0500

    Update DevScript.js

--- DevScript.js
+++ DevScript.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  http.get('http://192.168.100.10:1234');
+  http.get('http://192.168.100.10:1235');
   sleep(1);
 }
