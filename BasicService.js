87c3f28ee409ebf36b94b74f6348401c62eb5d91
Señor Performo - Leandro Melendez <54183040+srperf@users.noreply.github.com>
2022-08-02 18:53:54 -0500

    Update BasicService.js

--- BasicService.js
+++ BasicService.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
     res.write('Hola\n');
     setInterval(function() {
             res.end(' el World\n');
-        },between(6000,9500));
+        },between(1000,1500));
 });
 
 function between(min, max) {  
