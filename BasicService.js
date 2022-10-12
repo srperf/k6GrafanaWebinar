38fb43b57aad36bfe753b0c75231b3b4a78bb13e
Señor Performo - Leandro Melendez <54183040+srperf@users.noreply.github.com>
2022-10-12 10:51:52 -0700

    Update BasicService.js

--- BasicService.js
+++ BasicService.js
@@ -3,14 +3,14 @@ const http = require('http');
 //const hostname = '192.168.100.19';
 const hostname = 'localhost';
 let initTime = Date.now();
-const port = 1235;
+const port = 1234;
 //await new Promise(resolve => setTimeout(resolve, 5000));
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('Hola\n' + initTime);
     setInterval(function() {
             res.end('\n Mitad\n');
-        },between(5000,10000));
+        },between(500,1000));
     //res.end('\n el World\n');
 });
 
